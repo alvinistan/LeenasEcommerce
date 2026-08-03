4ea7d6862233a0f36595072d77fb33310888055f
alvinistan <alvinistan@students.nsbm.ac.lk>
2026-08-03 21:30:40 +0530

    first commit

---
+++ leenasCart/src/main/java/com/leenas/leenasCart/LeenasCartApplication.java
@@ -0,0 +1,13 @@
+package com.leenas.leenasCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeenasCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeenasCartApplication.class, args);
+	}
+
+}
---
+++ leenasCart/src/test/java/com/leenas/leenasCart/LeenasCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leenas.leenasCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeenasCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
